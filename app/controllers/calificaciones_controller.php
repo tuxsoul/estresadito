ef761d48cadc885ccf5b44b9afd0b5ddf639a1da
Mario Oyorzabal Salgado <tuxsoul@tuxsoul.com>
2009-06-30 01:24:32 -0500

    arreglado calificaciones.
    Signed-off-by: Mario Oyorzabal Salgado <tuxsoul@tuxsoul.com>

--- app/controllers/calificaciones_controller.php
+++ app/controllers/calificaciones_controller.php
@@ -4,23 +4,50 @@ class CalificacionesController extends AppController {
 	var $name = 'Calificaciones';
 	var $helpers = array('Html', 'Form' );
 
-	function index() {
-		$this->Calificacione->recursive = 0;
-		$this->set('calificaciones', $this->Calificacione->findAll());
-	}
+	function index($formato = null, $id = null) {
+		if(!empty($id) && !empty($formato)) {
+			if($formato) {
+				$tmp = $this->Calificacione->AlumnosGrupo->Grupo->findById($id);
+				$grado = $tmp['Grupo']['grado'];
+				$cicloEscolar = $tmp['Grupo']['ciclos_escolares_id'];
 
-	function view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash('Invalid id for Calificacione.');
-			$this->redirect('/calificaciones/index');
+				$this->set('grado', $grado);
+				$this->set('grupo', $tmp['Grupo']['grupo']);
+
+				$condicionMaterias = array( 'MaestrosMateria.ciclos_escolares_id' => $cicloEscolar,
+							    'MaestrosMateria.grado'		  => $grado 
+							);
+				$materias = $this->Calificacione->MaestrosMateria->findAll($condicionMaterias, null, null, null, null, 1);
+				$this->set('materias', $materias);
+
+				$alumnosGrupos = $this->Calificacione->AlumnosGrupo->findByGrupos_Id($id);
+				$condicionCalificaciones = array('Calificacione.alumnos_grupos_id' => $alumnosGrupos['AlumnosGrupo']['id']);
+				$calificaciones = $this->Calificacione->findAll($condicionCalificaciones, null, null, null, null, 1);
+				$this->set('calificaciones', $calificaciones);
+				$this->set('alumnosGrupos', $alumnosGrupos['AlumnosGrupo']['id']);
+
+				$alumno = $this->Calificacione->AlumnosGrupo->Alumno->findById($alumnosGrupos['AlumnosGrupo']['alumnos_id']);
+				$this->set('alumno', $alumno);
+
+				$nivelEscolar = $this->Calificacione->AlumnosGrupo->Grupo->CiclosEscolare->findById($cicloEscolar);
+				$this->set('nivelEscolar', $nivelEscolar['NivelesEscolare']['valor']);
+				$this->set('cicloEscolar', $nivelEscolar['CiclosEscolare']['inicio'] . '-' . $nivelEscolar['CiclosEscolare']['fin']);
+			}
+			else {
+
+			}
+
+			$this->set('formato', $formato);
 		}
-		$this->set('calificacione', $this->Calificacione->read(null, $id));
+		else {
+			$this->redirect('/');
+		}
 	}
 
-	function add() {
+	function add($alumnos = null, $materias = null) {
 		if (empty($this->data)) {
-			$this->set('alumnosGrupos', $this->Calificacione->AlumnosGrupo->generateList());
-			$this->set('maestrosMaterias', $this->Calificacione->MaestrosMateria->generateList());
+			$this->set('alumnos', $alumnos);
+			$this->set('materias', $materias);
 			$this->render();
 		} else {
 			$this->cleanUpFields();
@@ -29,8 +56,8 @@ class CalificacionesController extends AppController {
 				$this->redirect('/calificaciones/index');
 			} else {
 				$this->Session->setFlash('Please correct errors below.');
-				$this->set('alumnosGrupos', $this->Calificacione->AlumnosGrupo->generateList());
-				$this->set('maestrosMaterias', $this->Calificacione->MaestrosMateria->generateList());
+				$this->set('alumnos', $alumnos);
+				$this->set('materias', $materias);
 			}
 		}
 	}
@@ -42,8 +69,6 @@ class CalificacionesController extends AppController {
 				$this->redirect('/calificaciones/index');
 			}
 			$this->data = $this->Calificacione->read(null, $id);
-			$this->set('alumnosGrupos', $this->Calificacione->AlumnosGrupo->generateList());
-			$this->set('maestrosMaterias', $this->Calificacione->MaestrosMateria->generateList());
 		} else {
 			$this->cleanUpFields();
 			if ($this->Calificacione->save($this->data)) {
@@ -51,8 +76,6 @@ class CalificacionesController extends AppController {
 				$this->redirect('/calificaciones/index');
 			} else {
 				$this->Session->setFlash('Please correct errors below.');
-				$this->set('alumnosGrupos', $this->Calificacione->AlumnosGrupo->generateList());
-				$this->set('maestrosMaterias', $this->Calificacione->MaestrosMateria->generateList());
 			}
 		}
 	}
